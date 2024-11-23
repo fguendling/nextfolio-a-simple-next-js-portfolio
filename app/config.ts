17d5c88a69ec379e36e2681acfef959a17befa8e
Felix Gündling <46090606+fguendling@users.noreply.github.com>
2024-11-23 18:14:40 +0000

    Update title

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
+  title: "Felix Gündling",
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
